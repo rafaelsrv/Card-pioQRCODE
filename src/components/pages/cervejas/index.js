5d668ebbfa9b8b89336df04578a30f65c663ec10
Rafael <rafael-srv@hotmail.com>
2022-10-31 23:01:25 -0300

    Implementação dos espetos

--- src/components/pages/cervejas/index.js
+++ src/components/pages/cervejas/index.js
@@ -3,7 +3,21 @@ import styles from './cervejas.module.css'
 function Cervejas() {
     return (
         <div className={styles.fundo} >
+            <div></div>
+            <div className={styles.div_list}>
+                <li className={styles.titulo_list}>ESPETOS </li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
             
+            </div>
 
             
         </div>
