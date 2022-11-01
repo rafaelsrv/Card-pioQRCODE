c300e5b12bec7dcc89c6639fa965d18f62c86a2c
Rafael <rafael-srv@hotmail.com>
2022-11-01 00:02:00 -0300

    Implementando página de cervejas

--- src/components/pages/cervejas/index.js
+++ src/components/pages/cervejas/index.js
@@ -6,16 +6,16 @@ function Cervejas() {
            
             <div className={styles.div_list}>
                 <li className={styles.titulo_list}>CERVEJAS </li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>HEINIKEN LONG NECK . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CORONA LONG NECK . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .. . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>SKOLL ABRE FÁCIL. . . . . . . . . . . . . . . . . . . . . . . . . .  . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>SOLL BUCHUDINHA (300ML) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>BOHEMIA BUCHUDINHA (300ML) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>SKOLL LATA(355ML) . . . . . . . . . . . . . . . .  . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>ITAIPAVA LATA(355ML) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
             
             </div>
 
