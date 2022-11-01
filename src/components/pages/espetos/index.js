39b69d932d11851c4c44cc364878a69c525ba3ab
Rafael <rafael-srv@hotmail.com>
2022-11-01 00:08:36 -0300

    Implementando espetos

--- src/components/pages/espetos/index.js
+++ src/components/pages/espetos/index.js
@@ -6,9 +6,9 @@ function espetos() {
             <div></div>
             <div className={styles.div_list}>
                 <li className={styles.titulo_list}>ESPETOS </li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CARNE . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CARNE COM BACON . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>MEDALHÃO(CARNE+BACON) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
                 <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
                 <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
                 <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
