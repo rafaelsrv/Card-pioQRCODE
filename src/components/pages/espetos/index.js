4cf555c988e00d093b859381ed76761d517494ea
Rafael <rafael-srv@hotmail.com>
2022-11-01 00:13:48 -0300

    Implementando Espetos

--- src/components/pages/espetos/index.js
+++ src/components/pages/espetos/index.js
@@ -6,16 +6,18 @@ function espetos() {
             <div></div>
             <div className={styles.div_list}>
                 <li className={styles.titulo_list}>ESPETOS </li>
-                <li className={styles.itens_list}>CARNE . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CARNE . . . . . . . . . . . . . . . . . . . . . . . . . . . . .  . . .  . . . . . .  . . . .. . . . . . . . . . . . R$ 6,00</li>
                 <li className={styles.itens_list}>CARNE COM BACON . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>MEDALHÃO(CARNE+BACON) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
-                <li className={styles.itens_list}>Espeto de negocio . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>MEDALHÃO(CARNE+BACON) . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>FRANGO . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>SOBRECOXA DE FRANGO . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>ASINHA DE FRANGO . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CORAÇÃO DE FRANGO . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>PORCO COM BACON . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>PORCO . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .  . . . . .  . . . . . .  . .. R$ 6,00</li>
+                <li className={styles.itens_list}>TRIPA . . . . . . . . . . . . . . . . . . . . . .  . . . . .  . . . .  . . . . .. . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                <li className={styles.itens_list}>CORAÇÃO DE BOI . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . R$ 6,00</li>
+                
             
             </div>
 
